f628bb49e06ef0970f97e5aeee1e4f1650ca4dc9
supeng <supeng13@huawei.com>
2022-03-27 15:47:34 +0800

    update settings code
    Signed-off-by: supeng <supeng13@huawei.com>

---
+++ common/search/src/main/ets/default/common/ResourceUtil.ts
@@ -0,0 +1,50 @@
+// @ts-nocheck
+/**
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import ConfigData from '../../../../../../utils/src/main/ets/default/baseUtil/ConfigData';
+import ResMgr from '@ohos.resourceManager';
+import Log from '../../../../../../utils/src/main/ets/default/baseUtil/LogDecorator';
+
+/**
+ * Resource util
+ */
+export class ResourceUtil {
+  private resMgr: ResMgr.ResourceManager;
+
+  /**
+   * Initialize ResourceManager
+   */
+  @Log
+  async initResourceManager(): Promise<void> {
+    if (!this.resMgr) {
+      this.resMgr = await ResMgr.getResourceManager(ConfigData.DEFAULT_BUNDLE_NAME);
+    }
+  }
+
+  /**
+   * Get string value from NormalResource instance
+   *
+   * @param resource - NormalResource instance
+   */
+  @Log
+  async getString(resource): Promise<string> {
+    await this.initResourceManager();
+    return await this.resMgr.getString(resource.id);
+  }
+
+}
+
+let resourceUtil = new ResourceUtil();
+export default resourceUtil as ResourceUtil;
