8267b7d78f2b78b0d9fe2357aaf555e03606d82e
weiyunxing <weiyunxing@huawei.com>
2022-08-16 15:07:44 +0800

    蓝牙、NFC优化，fullSDK适配
    Signed-off-by: weiyunxing <weiyunxing@huawei.com>

--- hvigorfile.js
+++ hvigorfile.js
@@ -12,4 +12,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-module.exports = require('@ohos/hvigor-ohos-plugin').appTasks
+module.exports = require('@ohos/hvigor-ohos-plugin').appTasks;
