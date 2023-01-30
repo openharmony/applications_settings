f40cd9826c7dbae09ac364c6a2a3ff04bdada867
fangzheng24 <fangzheng24@huawei.com>
2023-01-30 01:45:38 +0000

    update napi/settings/napi_settings_log.h.
    Signed-off-by: fangzheng24 <fangzheng24@huawei.com>

--- napi/settings/napi_settings_log.h
+++ napi/settings/napi_settings_log.h
@@ -41,10 +41,9 @@
 #undef LOG_ERROR
 #endif
 
-namespace ohos {
-namespace settings {
-    static constexpr OHOS::HiviewDFX::HiLogLabel SETTINGS_LABEL = {LOG_CORE,0xd01900,"Settings"};
-    #define __FILENAME__ (__builtin_strrchr (__FILE__,'/') ? __builtin_strrchr (__FILE__,'/')+1:__FILE__) 
+namespace OHOS::Settings {
+    static const OHOS::HiviewDFX::HiLogLabel SETTINGS_LABEL = {LOG_CORE,0xd01900,"Settings"};
+    #define __FILENAME__ (__builtin_strrchr(__FILE__,'/') ? __builtin_strrchr (__FILE__,'/')+1:__FILE__) 
     #define LOG_DEBUG(fmt,...)\
     (void)OHOS::HiviewDFX::HiLog::Debug(\
     SETTINGS_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
@@ -60,6 +59,5 @@ namespace settings {
     #define LOG_ERROR(fmt,...)\
     (void)OHOS::HiviewDFX::HiLog::Error(\
     SETTINGS_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
-}  // namespace Settings
 }  // namespace OHOS
 #endif  //  NAPI_SETTINGS_LOG_H
