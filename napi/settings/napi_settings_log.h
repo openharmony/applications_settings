c6943e60ceba29d397e25430e8496a362eac99ce
fangzheng24 <fangzheng24@huawei.com>
2023-01-31 07:00:05 +0000

    update napi/settings/napi_settings_log.h.
    Signed-off-by: fangzheng24 <fangzheng24@huawei.com>

--- napi/settings/napi_settings_log.h
+++ napi/settings/napi_settings_log.h
@@ -21,55 +21,55 @@
 #ifdef CONFIG_HILOG
 #include "hilog/log.h"
 
-#ifdef LOG_DEBUG
-#undef LOG_DEBUG
+#ifdef SETTING_LOG_DEBUG
+#undef SETTING_LOG_DEBUG
 #endif
 
-#ifdef LOG_INFO
-#undef LOG_INFO
+#ifdef SETTING_LOG_INFO
+#undef SETTING_LOG_INFO
 #endif
 
-#ifdef LOG_WARN
-#undef LOG_WARN
+#ifdef SETTING_LOG_WARN
+#undef SETTING_LOG_WARN
 #endif
 
-#ifdef LOG_FATAL
-#undef LOG_FATAL
+#ifdef SETTING_LOG_FATAL
+#undef SETTING_LOG_FATAL
 #endif
 
-#ifdef LOG_ERROR
-#undef LOG_ERROR
+#ifdef SETTING_LOG_ERROR
+#undef SETTING_LOG_ERROR
 #endif
 
-#ifdef LOG_LABEL
-#undef LOG_LABEL
+#ifdef SETTING_LOG_LABEL
+#undef SETTING_LOG_LABEL
 #endif
 
-static constexpr OHOS::HiviewDFX::HiLogLabel LOG_LABEL = {LOG_CORE,0xD001900,"Settings"};
+static constexpr OHOS::HiviewDFX::HiLogLabel SETTING_LOG_LABEL = {LOG_CORE,0xD001900,"Settings"};
 #define __FILENAME__ (__builtin_strrchr(__FILE__,'/') ? __builtin_strrchr (__FILE__,'/')+1:__FILE__) 
-#define LOG_DEBUG(fmt,...)\
+#define SETTING_LOG_DEBUG(fmt,...)\
 (void)OHOS::HiviewDFX::HiLog::Debug(\
-LOG_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
-#define LOG_INFO(fmt,...)\
+SETTING_LOG_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
+#define SETTING_LOG_INFO(fmt,...)\
 (void)OHOS::HiviewDFX::HiLog::Info(\
-LOG_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
-#define LOG_WARN(fmt,...)\
+SETTING_LOG_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
+#define SETTING_LOG_WARN(fmt,...)\
 (void)OHOS::HiviewDFX::HiLog::Warn(\
-LOG_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
-#define LOG_FATAL(fmt,...)\
+SETTING_LOG_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
+#define SETTING_LOG_FATAL(fmt,...)\
 (void)OHOS::HiviewDFX::HiLog::Fatal(\
-LOG_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
-#define LOG_ERROR(fmt,...)\
+SETTING_LOG_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
+#define SETTING_LOG_ERROR(fmt,...)\
 (void)OHOS::HiviewDFX::HiLog::Error(\
-LOG_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
+SETTING_LOG_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
 
 #else
 
-#define LOG_DEBUG(...)
-#define LOG_INFO(...)
-#define LOG_WARN(...)
-#define LOG_FATAL(...)
-#define LOG_ERROR(...)
+#define SETTING_LOG_DEBUG(...)
+#define SETTING_LOG_INFO(...)
+#define SETTING_LOG_WARN(...)
+#define SETTING_LOG_FATAL(...)
+#define SETTING_LOG_ERROR(...)
 #endif //CONFIG_HILOG
 
 #endif //NAPI_SETTINGS_LOG_H
