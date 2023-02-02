33d3507f019ec1fcf56f7d041dbcaa4cdb5691bb
fangzheng24 <fangzheng24@huawei.com>
2023-02-02 03:35:22 +0000

    update napi/settings/napi_settings_log.h.
    Signed-off-by: fangzheng24 <fangzheng24@huawei.com>

--- napi/settings/napi_settings_log.h
+++ napi/settings/napi_settings_log.h
@@ -16,29 +16,32 @@
 #ifndef NAPI_SETTINGS_LOG_H
 #define NAPI_SETTINGS_LOG_H
 
-#ifdef LOG_TAG
+#ifndef LOG_TAG
 #define LOG_TAG
 #endif
 
 #include "hilog/log.h"
 
-namespace ohos {
-namespace settings {
-static const OHOS::HiviewDFX::HiLogLabel SETTING_LABEL = {LOG_CORE,0xD001900,"Settings"};
-#define __FILENAME__ (__builtin_strrchr(__FILE__,'/') ? __builtin_strrchr (__FILE__,'/')+1:__FILE__) 
-#define SETTING_LOG_DEBUG(fmt,...)\
-(void)OHOS::HiviewDFX::HiLog::Debug(SETTING_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
-#define SETTING_LOG_INFO(fmt,...)\
-(void)OHOS::HiviewDFX::HiLog::Info(SETTING_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
-#define SETTING_LOG_WARN(fmt,...)\
-(void)OHOS::HiviewDFX::HiLog::Warn(SETTING_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
-#define SETTING_LOG_FATAL(fmt,...)\
-(void)OHOS::HiviewDFX::HiLog::Fatal(SETTING_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
-#define SETTING_LOG_ERROR(fmt,...)\
-(void)OHOS::HiviewDFX::HiLog::Error(SETTING_LABEL,"[%{public}s(%{public}s:%{public}d)]" fmt,__FILENAME__,__FUNCTION__,__LINE__,##__VA_ARGS__)
-}  // namespace Settings
-}  // namespace OHOS
+namespace OHOS::Settings {
+static const OHOS::HiviewDFX::HiLogLabel SETTINGS_LABEL = { LOG_CORE, 0xD001900, "Settings" };
 
-#endif 
+#define __FILENAME__ (__builtin_strrchr(__FILE__, '/') ? __builtin_strrchr(__FILE__, '/') + 1 : __FILE__)
 
+#define SETTING_LOG_DEBUG(fmt, ...) \
+    (void)OHOS::HiviewDFX::HiLog::Debug(SETTINGS_LABEL, \
+    "[%{public}s(%{public}s:%{public}d)]" fmt, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define SETTING_LOG_INFO(fmt, ...) \
+    (void)OHOS::HiviewDFX::HiLog::Info(SETTINGS_LABEL, \
+    "[%{public}s(%{public}s:%{public}d)]" fmt, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define SETTING_LOG_WARN(fmt, ...) \
+    (void)OHOS::HiviewDFX::HiLog::Warn(SETTINGS_LABEL, \
+    "[%{public}s(%{public}s:%{public}d)]" fmt, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define SETTING_LOG_ERROR(fmt, ...) \
+    (void)OHOS::HiviewDFX::HiLog::Error(SETTINGS_LABEL, \
+    "[%{public}s(%{public}s:%{public}d)]" fmt, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define SETTING_LOG_FATAL(fmt, ...) \
+    (void)OHOS::HiviewDFX::HiLog::Fatal(SETTINGS_LABEL, \
+    "[%{public}s(%{public}s:%{public}d)]" fmt, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+} // namespace OHOS::DataShare
+#endif
 
