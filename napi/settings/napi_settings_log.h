ce89bb49f53ac901bee2ca58f6af9eafac77460d
侯志雄 <houzhixiong1@h-partners.com>
2024-10-12 15:38:07 +0800

    LOG_CORE整改
    Signed-off-by: 侯志雄 <houzhixiong1@h-partners.com>

--- napi/settings/napi_settings_log.h
+++ napi/settings/napi_settings_log.h
@@ -23,25 +23,24 @@
 #include "hilog/log.h"
 
 namespace OHOS::Settings {
-static const OHOS::HiviewDFX::HiLogLabel SETTINGS_LABEL = { LOG_APP, 0x0500, "Settings" };
+static const OHOS::HiviewDFX::HiLogLabel SETTINGS_LABEL = { LOG_CORE, 0xD000501, "Settings" };
 
 #define __FILENAME__ (__builtin_strrchr(__FILE__, '/') ? __builtin_strrchr(__FILE__, '/') + 1 : __FILE__)
 
 #define SETTING_LOG_DEBUG(fmt, ...) \
-    (void)OHOS::HiviewDFX::HiLog::Debug(SETTINGS_LABEL, \
+    HiLogDebug(SETTINGS_LABEL, \
     "[%{public}s(%{public}s:%{public}d)]" fmt, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #define SETTING_LOG_INFO(fmt, ...) \
-    (void)OHOS::HiviewDFX::HiLog::Info(SETTINGS_LABEL, \
+    HiLogInfo(SETTINGS_LABEL, \
     "[%{public}s(%{public}s:%{public}d)]" fmt, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #define SETTING_LOG_WARN(fmt, ...) \
-    (void)OHOS::HiviewDFX::HiLog::Warn(SETTINGS_LABEL, \
+    HiLogWarn(SETTINGS_LABEL, \
     "[%{public}s(%{public}s:%{public}d)]" fmt, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #define SETTING_LOG_ERROR(fmt, ...) \
-    (void)OHOS::HiviewDFX::HiLog::Error(SETTINGS_LABEL, \
+    HiLogError(SETTINGS_LABEL, \
     "[%{public}s(%{public}s:%{public}d)]" fmt, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #define SETTING_LOG_FATAL(fmt, ...) \
-    (void)OHOS::HiviewDFX::HiLog::Fatal(SETTINGS_LABEL, \
+    HiLogFatal(SETTINGS_LABEL, \
     "[%{public}s(%{public}s:%{public}d)]" fmt, __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 } // namespace OHOS::Settings
 #endif
-
