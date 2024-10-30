15b9424ecf9411f316e8960cc6274bb3a88b54a7
侯志雄 <houzhixiong1@h-partners.com>
2024-10-30 11:36:00 +0800

    dataShareHelper创建添加等待参数
    Signed-off-by: 侯志雄 <houzhixiong1@h-partners.com>

--- napi/settings/napi_settings.cpp
+++ napi/settings/napi_settings.cpp
@@ -41,6 +41,7 @@ const std::string PERMISSION_EXCEPTION = "201 - Permission denied";
 const int PERMISSION_DENIED_CODE = -2;
 const int DB_HELPER_TRIAL_NUMBER = 2;
 const int USERID_HELPER_NUMBER = 100;
+const int WAIT_TIME = 5;
 
 void ThrowExistingError(napi_env env, std::string errorMessage)
 {
@@ -394,7 +395,7 @@ void CheckDataShareHelper(napi_env env, const napi_value context,
             SETTING_LOG_INFO("settingsnapi : getDataShareHelper resultset == nullptr, strUri %{public}s %{public}d",
                 strUri.c_str(),
                 trial);
-            dataShareHelper = OHOS::DataShare::DataShareHelper::Creator(contextS->GetToken(), strUri, waitTime = 5);
+            dataShareHelper = OHOS::DataShare::DataShareHelper::Creator(contextS->GetToken(), strUri, WAIT_TIME);
         } while (trial++ < DB_HELPER_TRIAL_NUMBER && dataShareHelper == nullptr);
         if (resultset != nullptr) {
             resultset->Close();
@@ -426,7 +427,7 @@ std::shared_ptr<DataShareHelper> getDataShareHelper(
         return dataShareHelper;
     }
 
-    dataShareHelper = OHOS::DataShare::DataShareHelper::Creator(contextS->GetToken(), strProxyUri, strUri, waitTime = 5);
+    dataShareHelper = OHOS::DataShare::DataShareHelper::Creator(contextS->GetToken(), strProxyUri, strUri, WAIT_TIME);
     SETTING_LOG_INFO("g_D_S_H Creator called, valid %{public}d", dataShareHelper != nullptr);
     return dataShareHelper;
 }
