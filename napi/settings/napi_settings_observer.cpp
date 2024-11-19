aea7f4ac9a550a484f20c2c8a52fe7f845537a58
侯志雄 <houzhixiong1@h-partners.com>
2024-11-19 16:16:59 +0800

    完善获取用户id逻辑，user0使用当前用户空间获取用户id
    Signed-off-by: 侯志雄 <houzhixiong1@h-partners.com>

--- napi/settings/napi_settings_observer.cpp
+++ napi/settings/napi_settings_observer.cpp
@@ -121,8 +121,13 @@ namespace Settings {
         int currentUserId = -1;
         OHOS::AccountSA::OsAccountManager::GetOsAccountLocalIdFromProcess(currentUserId);
         std::string tmpIdStr = "100";
-        if (currentUserId >= 0) {
+        if (currentUserId > 0) {
             tmpIdStr = std::to_string(currentUserId);
+            SETTING_LOG_INFO("userId is %d", currentUserId);
+        } else if (currentUserId == 0) {
+            OHOS::AccountSA::OsAccountManager::GetForegroundOsAccountLocalId(currentUserId);
+            tmpIdStr = std::to_string(currentUserId);
+            SETTING_LOG_INFO("user0 userId is %d", currentUserId);
         } else {
             SETTING_LOG_INFO("%{public}s, user id 100.", __func__);
         }
