46f6fbf572b1a2c51f12760d64604ad1399f5fca
侯志雄 <houzhixiong1@h-partners.com>
2024-12-18 03:49:07 +0000

    update napi/settings/open_network_settings/napi_open_network_settings.h.
    Signed-off-by: 侯志雄 <houzhixiong1@h-partners.com>

--- napi/settings/open_network_settings/napi_open_network_settings.h
+++ napi/settings/open_network_settings/napi_open_network_settings.h
@@ -43,7 +43,7 @@ struct SettingsError {
 
 const SettingsError SETTINGS_ERROR_PARAM = {SETTINGS_PARAM_ERROR_CODE, "Parameter error."};
 const SettingsError SETTINGS_ERROR_ORIGINAL_SERVICE = {SETTINGS_ORIGINAL_SERVICE_CODE, "Original service error."};
-const std::map<SettingsCode, SettingsError> ERROR_MAP = {
+const std::map<SettingsCode, SettingsError> g_errorMap = {
     {SETTINGS_PARAM_ERROR, SETTINGS_ERROR_PARAM},
     {SETTINGS_ORIGINAL_SERVICE_ERROR, SETTINGS_ERROR_ORIGINAL_SERVICE}
 };
