fae6fb6fb5ecb3024dbabc53fc4ef24744e15d54
liangweiwei <liangweiwei3@huawei.com>
2025-10-18 18:05:07 +0800

    wifi连接异常问题优化
    Signed-off-by: liangweiwei <liangweiwei3@huawei.com>

--- product/phone/src/main/ets/model/wifiImpl/WifiModel.ts
+++ product/phone/src/main/ets/model/wifiImpl/WifiModel.ts
@@ -394,7 +394,7 @@ export class WifiModel extends BaseModel {
       return;
     }
     LogUtil.info(MODULE_TAG + 'start to removeDevice');
-    LogUtil.info(MODULE_TAG + 'remove networkId: ' + networkId + ' ssid: ' + apInfo.ssid);
+    LogUtil.info(MODULE_TAG + 'remove ssid: ' + LogUtil.dealLog(apInfo.ssid));
     let ret = wifi.removeDevice(networkId);
     LogUtil.info(MODULE_TAG + 'remove device config : ' + ret);
   }
