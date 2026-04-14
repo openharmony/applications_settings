578185a5f6d7ae42f502ae932bbaeadffd67bb02
shenkang6 <shenkang6@huawei.com>
2026-04-14 16:07:39 +0800

    feat:新增settings api接口实现
    Signed-off-by: shenkang6 <shenkang6@huawei.com>

--- napi/settings/open_network_settings/napi_open_network_settings.h
+++ napi/settings/open_network_settings/napi_open_network_settings.h
@@ -76,6 +76,10 @@ napi_value openInputMethodDetail(napi_env env, napi_callback_info info);
 napi_value OpenNfcSettingsPage(napi_env env, napi_callback_info info);
 napi_value OpenBiometricsSettingsPage(napi_env env, napi_callback_info info);
 napi_value OpenAboutDeviceSettingsPage(napi_env env, napi_callback_info info);
+napi_value OpenMobileNetworkSettingsPage(napi_env env, napi_callback_info info);
+napi_value OpenDisplaySettingsPage(napi_env env, napi_callback_info info);
+napi_value OpenScreenRefreshRateSettingsPage(napi_env env, napi_callback_info info);
+napi_value OpenSoundSettingsPage(napi_env env, napi_callback_info info);
 napi_value IsDoubleClickAppForSelf(napi_env env, napi_callback_info info);
 napi_value OpenDoubleClickSettingsPage(napi_env env, napi_callback_info info);
 }
